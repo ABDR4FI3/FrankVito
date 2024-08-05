d636c5ba10f8dd51e29685e6f4768803aa6475ab
ABDR4FI3 <its.abdrafi3@gmail.com>
2024-08-05 11:48:13 +0100

    Almost Fully Responsive

---
+++ src/Script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("[data-fancybox]").fancybox({
+    // Options can go here
+    iframe: {
+      css: {
+        width: "80%",
+        height: "80%",
+      },
+    },
+  });
+});
